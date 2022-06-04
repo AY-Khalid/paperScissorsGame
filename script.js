94cb7f0fc5de12d3f760319c4332164919e49048
AYKhalid <aniduyakubu@yahoo.com>
2022-06-04 16:59:11 +0100

    second commit

--- script.js
+++ script.js
@@ -51,4 +51,24 @@ function rpsFrontEnd(humanImagesChoice, botImagesChoice, finalMessage) {
         "paper": document.getElementById("paper").src,
         "scissors": document.getElementById("scissors").src
     }
-}
+
+        // Do this to remove all the images on a click
+
+        document.getElementById("rock").remove();
+        document.getElementById("paper").remove();
+        document.getElementById("scissors").remove();
+    
+        var humanDiv = document.createElement("div");
+        var botDiv = document.createElement("div");
+        var messageDiv = document.createElement("div");
+    
+    
+    humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImagesChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 15px purple;'>"
+    messageDiv.innerHTML = "<h1 style='color:" + finalMessage['color'] + "; font-size: 60px; padding: 30px;'>" + finalMessage['message'] + "</h1>"
+    botDiv.innerHTML = "<img src='" + imagesDatabase[botImagesChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 15px blue;'>"
+    
+    document.getElementById("flex-box-rps-div").appendChild(humanDiv);
+    document.getElementById("flex-box-rps-div").appendChild(messageDiv);
+    document.getElementById("flex-box-rps-div").appendChild(botDiv);
+    
+    } 
